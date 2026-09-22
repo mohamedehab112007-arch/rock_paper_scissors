c37032919bd655b4d50e74b191d93803a1a99526
mohamedehab112007-arch <mohamed.ehab112007@gmail.com>
2026-09-22 19:45:08 +0300

    Add a new function generating the computer choise rock, paper , scissor.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+let computer_choise=null;
+function getComputerChoise()
+{
+  let random_num=Math.random();
+  if(random_num<0.3333 && random_num>=0)
+  {
+    computer_choise="paper"
+  }
+  else if(random_num>0.3333 && random_num<0.6666)
+  {
+   computer_choise="rock" 
+  }
+  else 
+  {
+   computer_choise="scissor"     
+  }
+}
+getComputerChoise();
+console.log(computer_choise);
