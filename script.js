120b28f62a9a4b0ccd37548e023904ffd6a5dfc1
mohamedehab112007-arch <mohamed.ehab112007@gmail.com>
2026-09-22 20:54:20 +0300

    Didn't save the function's logic so it wasn't addedto the commit

--- script.js
+++ script.js
@@ -25,7 +25,42 @@ function getHumanChoise()
 }
 function playRound(human_src,computer_src)
 {
-      computer_src=getComputerChoise()
-      human_src=getHumanChoise()
-      
+    computer_choise=getComputerChoise()
+    human_choise=getHumanChoise()
+    human_choise.toLowerCase()
+    if(human_choise==="rock" && computer_choise==="scissor")
+    {
+        human_src++;
+        console.log("YOU WIN!rock beats scissor")
+    }
+    else if(human_choise==="paper" && computer_choise==="rock")
+    {
+        human_src++;
+        console.log("YOU WIN!paper beats rock")
+    }
+    else if(human_choise==="scissor" && computer_choise==="paper")
+    {   
+        human_src++;
+        console.log("YOU WIN!scissor beats paper")         
+    }
+    else if(computer_choise==="rock" && human_choise==="scissor")
+    {
+        computer_src++
+        console.log("YOU LOSE!rock beats scissor")
+    }
+    else if(computer_choise==="paper" && human_choise==="rock")
+    {
+        computer_src++;
+        console.log("YOU LOSE!paper beats rock")
+    }
+    else if(computer_choise==="scissor" && human_choise==="paper")
+    {
+        computer_src++;
+        console.log("YOU LOSE!scissor beats paper")
+    }
+    else
+    {
+        console.log("TIE!")
+        
+    }
 }
